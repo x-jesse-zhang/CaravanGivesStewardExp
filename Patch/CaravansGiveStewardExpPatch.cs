3956a49592242ed90c950e019b60c7f6b8b82409
Jesse Zhang <macisasandwich@gmail.com>
2022-11-28 19:59:23 -0800

    Add project files.

---
+++ Patch/CaravansGiveStewardExpPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.CampaignSystem.Party;
+
+namespace CaravanGivesStewardExp
+{
+    [HarmonyPatch(typeof(DefaultMobilePartyFoodConsumptionModel), "DoesPartyConsumeFood")]
+    internal class CaravansGiveStewardExpPatch
+    {
+        static void Postfix(MobileParty mobileParty, ref bool __result)
+        {
+            __result = mobileParty.IsActive &&
+                   (mobileParty.LeaderHero == null || mobileParty.LeaderHero.IsLord || mobileParty.LeaderHero.Clan == Clan.PlayerClan || mobileParty.LeaderHero.IsMinorFactionHero) &&
+                   !mobileParty.IsGarrison &&
+                   !mobileParty.IsCommonAreaParty ||
+                   mobileParty.IsCaravan &&
+                   !mobileParty.IsBandit &&
+                   !mobileParty.IsMilitia &&
+                   !mobileParty.IsVillager;
+        }
+    }
+}
---
+++ SubModule.cs
@@ -0,0 +1,28 @@
+﻿using TaleWorlds.MountAndBlade;
+using HarmonyLib;
+
+namespace CaravanGivesStewardExp
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            Harmony h = new Harmony("CaravanGivesStewardExp");
+            h.PatchAll();
+
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
