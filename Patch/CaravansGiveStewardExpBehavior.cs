0316a2412a27568301d6448f708ba24e344c1a80
Jesse Zhang <macisasandwich@gmail.com>
2022-12-08 14:11:38 -0800

    1.1.0 Remove Harmony and fixed quest caravan bug

---
+++ Patch/CaravansGiveStewardExpBehavior.cs
@@ -0,0 +1,50 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.CampaignSystem.Party;
+
+namespace CaravanGivesStewardExp
+{
+    internal class CaravansGiveStewardExpBehavior : DefaultMobilePartyFoodConsumptionModel
+    {
+        public override int NumberOfMenOnMapToEatOneFood
+        {
+            get
+            {
+                return base.NumberOfMenOnMapToEatOneFood;
+            }
+        }
+
+        public override bool DoesPartyConsumeFood(MobileParty mobileParty)
+        {
+            if (mobileParty.LeaderHero != null && mobileParty.LeaderHero.Clan == Clan.PlayerClan)
+            {
+                if (mobileParty.IsCaravan)
+                {
+                    if (mobileParty.IsActive)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return mobileParty.IsActive && 
+                   (mobileParty.LeaderHero == null || mobileParty.LeaderHero.IsLord || mobileParty.LeaderHero.Clan == Clan.PlayerClan || mobileParty.LeaderHero.IsMinorFactionHero) && 
+                   !mobileParty.IsGarrison && 
+                   !mobileParty.IsCommonAreaParty && 
+                   !mobileParty.IsBandit && 
+                   !mobileParty.IsMilitia && 
+                   !mobileParty.IsVillager;
+        }
+
+        public override ExplainedNumber CalculateDailyBaseFoodConsumptionf(MobileParty party, bool includeDescription = false)
+        {
+            return base.CalculateDailyBaseFoodConsumptionf(party, includeDescription);
+        }
+
+        public override ExplainedNumber CalculateDailyFoodConsumptionf(MobileParty party, ExplainedNumber baseConsumption)
+        {
+            return base.CalculateDailyFoodConsumptionf(party, baseConsumption);
+        }
+
+    }
+}
